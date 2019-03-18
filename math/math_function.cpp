fc0ba9ae105dea0480d88381a33fe98e5893e55b
zhouhao <zhouhao@changba.com>
2019-03-18 10:56:00 +0800

    init learning cmake

---
+++ math/math_function.cpp
@@ -0,0 +1,19 @@
+
+#include "math_function.hpp"
+
+double power(double base, int exponent)
+{
+    int result = base;
+    int i;
+
+    if (exponent == 0)
+    {
+        return 1;
+    }
+
+    for(i = 1; i < exponent; ++i)
+    {
+        result = result * base;
+    }
+    return result;
+}
---
+++ math/math_function.hpp
@@ -0,0 +1,11 @@
+/**
+ * power - Calculate the power of number.
+ * @param base: Base value.
+ * @param exponent: Exponent value.
+ *
+ * @return base raised to the power exponent.
+ */
+#ifndef MATH_FUNCTION_HPP
+#define MATH_FUNCTION_HPP
+double power(double base, int exponent);
+#endif
